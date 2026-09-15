a178d70075f3c8c736eebe7d4830415fd44b7ff6
Nizar Y Alqerem <nizaryousef01@gmail.com>
2026-09-15 20:14:55 +0300

    fix: tolerate missing database URL during build

--- config/database.php
+++ config/database.php
@@ -86,7 +86,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => str_replace('postgresql://', 'pgsql://', env('DATABASE_URL', env('DB_URL'))),
+            'url' => str_replace('postgresql://', 'pgsql://', (string) env('DATABASE_URL', env('DB_URL'))),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
