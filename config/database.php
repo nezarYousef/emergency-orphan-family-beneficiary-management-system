6c0f2dbb1f72b72c368dccfb4e480b215734ac66
Nizar Y Alqerem <nizaryousef01@gmail.com>
2026-09-15 20:03:02 +0300

    fix: normalize Neon PostgreSQL URL scheme for Laravel

--- config/database.php
+++ config/database.php
@@ -34,7 +34,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL', env('DB_URL')),
+            'url' => env('DB_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
@@ -86,7 +86,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL', env('DB_URL')),
+            'url' => str_replace('postgresql://', 'pgsql://', env('DATABASE_URL', env('DB_URL'))),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
